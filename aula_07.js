98413979cded53cee609793c9ffcacfd08aa6cc9
Eduardo Lima <limaeduardo092@gmail.com>
2023-06-07 23:14:38 -0300

    Subindo branch

--- aula_07.js
+++ aula_07.js
@@ -1,3 +1,15 @@
-//&& -> and e
-//|| -> or ou
-//! -> not não
+// && -> and e
+// || -> or ou
+// ! -> not não
+
+let n1, n2, n3, n4;
+n1 = 10;
+n2 = 5;
+n3 = 15;
+n4 = 2;
+
+//console.log((n1>n2)&&(n1>n3)); //como ela é regida pelo AND resultado true e false
+
+//console.log((n1>n2)||(n1>n3));  // Ela é regida pelo OU o resultado e true e true
+
+console.log(!((n1>n2)||(n1>n3))); //oerador de negação ou inversão
