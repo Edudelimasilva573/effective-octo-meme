28a07c0d544028da135ff17814af65383529a0c4
Eduardo Lima <limaeduardo092@gmail.com>
2023-06-08 06:29:50 -0300

    add mudanças

--- aula_07.js
+++ aula_07.js
@@ -12,4 +12,15 @@ n4 = 2;
 
 //console.log((n1>n2)||(n1>n3));  // Ela é regida pelo OU o resultado e true e true
 
-console.log(!((n1>n2)||(n1>n3))); //oerador de negação ou inversão
+//console.log(!((n1>n2)||(n1>n3))); //operador de negação ou inversão
+
+/*if (n1 > n2) {
+  console.log(n1 + " maior que " + n2);
+} else {
+  console.log(n1 + " menor que " + n2);
+}*/
+if (!(n1 > n2) && (n3 > n4)) {
+  console.log("Verdadeiro");
+} else{
+  console.log("Falso");
+};
