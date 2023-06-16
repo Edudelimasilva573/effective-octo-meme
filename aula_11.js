8d5968a8d2bfa461e5e4bf8610dcb260583aa46c
Eduardo Lima <limaeduardo092@gmail.com>
2023-06-15 23:07:32 -0300

    adiciona novos Spread

--- aula_11.js
+++ aula_11.js
@@ -1,4 +1,9 @@
-let v1 = 10;
+let v1 = 10.10; 
 let v2 = "10";
-let v3 = 10;
-let v4 = 10;
+let v3 = v1 === v2;
+let v4 = {nome: "Eduardo"};
+
+console.log("Valor: " + v1 + " - tipo: " + typeof(v1));
+console.log("Valor: " + v2 + " - tipo: " + typeof (v2));
+console.log("Valor: " + v3 + " - tipo: " + typeof (v3));
+console.log("Valor: " + v4 + " - tipo: " + typeof (v4));
