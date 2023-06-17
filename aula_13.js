c1705e775ed4bc0aad09efa19e3d0292f8ea6362
Eduardo Lima <limaeduardo092@gmail.com>
2023-06-17 19:00:09 -0300

    adiciona novo exercicio

---
+++ aula_13.js
@@ -0,0 +1,9 @@
+let energia = 70
+let clima = "sol"
+
+if (energia > 70 && clima=="sol"){
+   console.log("Vou a praia")
+}else{
+  console.log("Vou ao cinema");
+}
+  console.log("Fim do Programa");
